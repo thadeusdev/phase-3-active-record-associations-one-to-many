e2028d41ac635c62ec5042b2c0164273697719cb
thadeus <thadeusnyariki94@gmail.com>
2022-11-03 02:17:11 +0300

    Done assignement

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,11 @@
 class Game < ActiveRecord::Base
-  
+  has_many :reviews
 end
+
+# class Game < ActiveRecord::Base
+
+#     def reviews
+#       Review.where(game_id: self.id)
+#     end
+  
+# end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,11 @@
+# class Review < ActiveRecord::Base
+#   # a review belongs to a game
+#   def game
+#     # self is the review instance
+#     Game.find(self.game_id)
+#   end
+# end
+
 class Review < ActiveRecord::Base
-  
+    belongs_to :game
 end
